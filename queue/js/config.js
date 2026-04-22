70c74376f30fcb7a7ea9404cb7cf688f9f6c7e48
Dmitry <dimabatashov@gmail.com>
2026-04-22 22:55:40 +0800

    minor terminal fix

--- queue/js/config.js
+++ queue/js/config.js
@@ -1,10 +1,10 @@
 // config.js
 const CONFIG = {
-    API_URL: "http://localhost:8000",
-    WS_TERMINAL_URL: "ws://localhost:8000/ws/terminal",
-    WS_BOARD_URL: "ws://localhost:8000/ws/board",
+    API_URL: "http://10.0.1.132:8000",
+    WS_TERMINAL_URL: "ws:/10.0.1.132:8000/ws/terminal",
+    WS_BOARD_URL: "ws://10.0.1.132:8000/ws/board",
     NOTICE_DURATION: 5,
     RECONNECT_INTERVAL: 2000,
-	GRAFANA_URL: "http://localhost:3000/goto/afg7a2bkgdce8a?orgId=1&panelId=1",
+	GRAFANA_URL: "http://10.0.1.132:3000/goto/3b8AI_2DR?orgId=1",
 	MEDIA_IDLE_DELAY: 15
 };
