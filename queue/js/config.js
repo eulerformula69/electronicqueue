deb61e29edcb8c082227d62a40e47b4b3a6907e8
Dmitry <dimabatashov@gmail.com>
2026-06-14 19:10:04 +0800

    fixed admin page styles

--- queue/js/config.js
+++ queue/js/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
     API_URL: "http://localhost:8000",
-    WS_TERMINAL_URL: "ws:/localhost:8000/ws/terminal",
+    WS_TERMINAL_URL: "ws://localhost:8000/ws/terminal",
     WS_BOARD_URL: "ws://localhost:8000/ws/board",
     NOTICE_DURATION: 7,
     RECONNECT_INTERVAL: 2000,
